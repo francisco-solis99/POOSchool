487f790a1919e78dfaf1f4369a713a2964f3bef9
francisco-solis99 <franciscosoliscss@gmail.com>
2022-03-08 21:32:02 -0600

    [Add] the Course class and the initial documentation

---
+++ Course.js
@@ -0,0 +1,38 @@
+// @ts-check
+/**
+ * Class to representate a Course
+ */
+class Course {
+   /**
+   * Course constructor
+   *
+   * @param {Object} obj - An object.
+   * @param {String} obj.name - Name of course
+   * @param {String} obj.teacher - Name of teacher's Course
+   * @param {Number} obj.modules - Number of modules of the course
+   * @param {Number} obj.timeToFinish - Time to finish the course in hours
+   * @param {Number} obj.rate - Rate of the course
+   */
+  constructor({
+    name,
+    teacher,
+    modules,
+    timeToFinish,
+    rate = 5,
+  })
+  {
+    this.name = name,
+    this.teacher = teacher,
+    this.modules = modules,
+    this.timeToFinish = timeToFinish,
+    this.rate = rate
+  }
+}
+
+const POOCourse = new Course({
+  name: 'POO with JS',
+  teacher: 'JuanDc',
+  modules: 4,
+  timeToFinish: 3,
+  rate: 5
+})
